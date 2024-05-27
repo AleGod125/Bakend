75be184ab02ebce7ca9d2c035237b39b37820910
AleGod125 <alejandroviolori@gmail.com>
2024-05-26 20:49:38 -0500

    Update api.py

--- api.py
+++ api.py
@@ -30,7 +30,7 @@ app = FastAPI()
 # Middleware CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://alegod125.github.io"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
